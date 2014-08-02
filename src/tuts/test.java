3c7323da0010e702e1a4fe2675361d0960f2af0c
Dhananjay Kumar <dhananjaykumar3781@gmail.com>
2014-08-03 03:15:29 +0530

    added sum

---
+++ src/tuts/test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tuts;
+
+/**
+ *
+ * @author DHANANJAY
+ */
+public class test {
+
+    public static void main(String[] args) {
+        int a = 0, b = 0;
+        int sum = a + b;
+        System.out.print(sum);
+
+    }
+}
